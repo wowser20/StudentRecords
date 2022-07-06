3fe53a25b65ff31b690a4ca37d554224deee1bc9
AndreaRemoreras <a.remoreras.512875@umindanao.edu.ph>
2022-07-06 23:36:32 +0800

    version1

---
+++ resources/views/students/edit.blade.php
@@ -0,0 +1,25 @@
+@extends('students.layout')
+@section('content')
+<button class="btn btn-dark" onclick="history.back()"> Back</button>  
+<p></p>
+<div class="card">
+  <div class="card-header">Edit Details</div>
+  
+  <div class="card-body">
+      
+      <form action="{{ url('student/' .$students->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
+        <label>Address</label></br>
+        <input type="text" name="address" id="address" value="{{$students->address}}" class="form-control"></br>
+        <label>Mobile</label></br>
+        <input type="text" name="mobile" id="mobile" value="{{$students->mobile}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+  
+  </div>
+</div>
+@stop
